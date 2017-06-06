44c06591cd8b520c04a9d3c45b46defaa2c7b8ac
Rails app <tmark_s@hotmail.com>
2017-06-06 16:43:35 +0700

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ root 'pages#home'
 get 'about', to: 'pages#about'
 
 resources :articles
+
+get 'signup', to: 'users#new'
+#post 'users', to:'users#create'
+resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
