b7d91cd739aa8c8da690f254fdeb663d7a2f40fb
book-store by tmark-s <tmark_s@hotmail.com>
2017-06-12 15:10:44 +0700

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-	
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
+
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
@@ -11,7 +14,7 @@ class ArticlesController < ApplicationController
 	def create
 		#debugger
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = User.last
 		if @article.save
 			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
@@ -21,11 +24,10 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit 
-		@article = Article.find(params[:id])
+
 	end
 
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			flash[:success] = "Article was successfully updated"
 			redirect_to article_path(@article)
@@ -35,18 +37,29 @@ class ArticlesController < ApplicationController
 	end
 
 	def show
-		@article = Article.find(params[:id])
+
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		flash[:danger] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 
 	private
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 	def article_params
 		params.require(:article).permit(:title, :description)
+	end
+
+	def require_same_user
+		if current_user != @article.user
+			flash[:danger] = "You can only edit or delete your
+			own articles"
+			redirect_to root_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ resources :articles
 get 'signup', to: 'users#new'
 #post 'users', to:'users#create'
 resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
