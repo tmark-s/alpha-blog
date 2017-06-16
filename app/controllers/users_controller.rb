5a52cfbf5d3d51253e1127f24cc9f5cad701d91e
book-store by tmark-s <tmark_s@hotmail.com>
2017-06-16 11:04:41 +0700

    Add sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,56 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_same_user, only: [:edit,:update]
+  before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit,:update]
 
-	def index
-		@users = User.paginate(page: params[:page], per_page: 5)
-	end
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to the alpha blog #{@user.username}"
-			redirect_to articles_path
-		else
-			render 'new'
-		end 
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end 
+  end
 
-	def edit
+  def edit
 
-	end
+  end
 
-	def update
-		if @user.update(user_params)
-			flash[:success] = "Your account was updated successfully"
-			redirect_to articles_path
-		else
-			render 'edit'
-		end
-	end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
 
-	def show
-		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
-	end
+  def show
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
 
-	private
-	def set_user
-		@user = User.find(params[:id])
-	end
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-	def user_params
-		params.require(:user).permit(:username, :email, :password)
-	end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-	def require_same_user
-		if current_user != @user
-			flash[:danger] = "You can only edit own account"
-			redirect_to root_path
-		end
-	end
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit own account"
+      redirect_to root_path
+    end
+  end
 end
