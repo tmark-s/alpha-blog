dfdd7c265f25f5c960c48d8ab7f12cbeabccbd91
book-store by tmark-s <tmark_s@hotmail.com>
2017-06-16 12:20:01 +0700

    Add admin user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606082356) do
+ActiveRecord::Schema.define(version: 20170616041217) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170606082356) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
 end
